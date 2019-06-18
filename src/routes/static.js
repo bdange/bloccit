e2c3fc9e78ff6bcf7b747aeb39ee17dae9dac74b
bdange <benjamin.dange@gmail.com>
2019-06-18 18:33:23 +0200

    work added

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const staticController = require("../controllers/staticController");
 
-router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccit");
-});
-
-router.get("/marco", (req, res, next) => {
-  res.send("Polo");
-});
+router.get("/", staticController.index);
+router.get("/about", staticController.about);
 
 module.exports = router;
