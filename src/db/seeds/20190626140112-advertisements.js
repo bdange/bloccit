730cd5801f10ec951a88786f61d8b5fb34498ed1
bdange <benjamin.dange@gmail.com>
2019-06-26 16:29:00 +0200

    improved

--- src/db/seeds/20190626140112-advertisements.js
+++ src/db/seeds/20190626140112-advertisements.js
@@ -1,21 +1,21 @@
-'use strict';
-	
-	const faker = require("faker");
-	
-	let advertisements = [];
-	
-	for(let i = 1 ; i <= 15 ; i++){
-	   advertisements.push({
-	     title: faker.hacker.noun(),
-	     description: faker.hacker.phrase(),
-	     createdAt: new Date(),
-	     updatedAt: new Date()
-	   });
-	}
-	
-	module.exports = {
-	  up: (queryInterface, Sequelize) => {
-	    /*
+"use strict";
+
+const faker = require("faker");
+
+let advertisements = [];
+
+for (let i = 1; i <= 15; i++) {
+  advertisements.push({
+    title: faker.hacker.noun(),
+    description: faker.hacker.phrase(),
+    createdAt: new Date(),
+    updatedAt: new Date()
+  });
+}
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
 	      Add altering commands here.
 	      Return a promise to correctly handle asynchronicity.
 	
@@ -25,16 +25,17 @@
 	        isBetaMember: false
 	      }], {});
 	    */
-	     return queryInterface.bulkInsert("Advertisements", advertisements, {});
-	  },
-	
-	  down: (queryInterface, Sequelize) => {
-	    /*
+    return queryInterface.bulkInsert("Advertisements", advertisements, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
 	      Add reverting commands here.
 	      Return a promise to correctly handle asynchronicity.
 	
 	      Example:
 	      return queryInterface.bulkDelete('Person', null, {});
 	    */
-	     return queryInterface.bulkDelete("Advertisements", null, {});
-	  }
+    return queryInterface.bulkDelete("Advertisements", null, {});
+  }
+};
