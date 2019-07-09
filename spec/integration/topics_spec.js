cbdc4a0d08bf832c72e03519314cd0e30191f1cd
bdange <benjamin.dange@gmail.com>
2019-07-09 19:27:57 +0200

    fixing issue

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -8,13 +8,13 @@ const User = require("../../src/db/models").User;
 describe("routes : topics", () => {
   beforeEach(done => {
     this.topic;
-    sequelize.sync({ force: true }).then(() => {
+    sequelize.sync({ force: true }).then(res => {
       Topic.create({
         title: "JS Frameworks",
-        description: "There are a lot of them"
+        description: "There is a lot of them"
       })
-        .then(res => {
-          this.topic = res;
+        .then(topic => {
+          this.topic = topic;
           done();
         })
         .catch(err => {
@@ -24,7 +24,7 @@ describe("routes : topics", () => {
     });
   });
 
-  //context of admin user
+  // define the admin user context
   describe("admin user performing CRUD actions for Topic", () => {
     beforeEach(done => {
       User.create({
@@ -50,7 +50,7 @@ describe("routes : topics", () => {
     });
 
     describe("GET /topics", () => {
-      it("should respond with all topics", done => {
+      it("should return a status code 200 and all topics", done => {
         request.get(base, (err, res, body) => {
           expect(err).toBeNull();
           expect(body).toContain("Topics");
@@ -61,43 +61,12 @@ describe("routes : topics", () => {
     });
 
     describe("GET /topics/new", () => {
-      it("should render a view with a new topic form", done => {
+      it("should render a new topic form", done => {
         request.get(`${base}new`, (err, res, body) => {
           expect(err).toBeNull();
           expect(body).toContain("New Topic");
           done();
         });
-      });
-    });
-
-    describe("POST /topics/create", () => {
-      const options = {
-        url: `${base}create`,
-        form: {
-          title: "blink-182 songs",
-          description: "What's your favorite blink-182 song?"
-        }
-      };
-
-      it("should create a new topic and redirect", done => {
-        request.post(
-          options,
-
-          (err, res, body) => {
-            Topic.findOne({ where: { title: "blink-182 songs" } })
-              .then(topic => {
-                expect(topic.title).toBe("blink-182 songs");
-                expect(topic.description).toBe(
-                  "What's your favorite blink-182 song?"
-                );
-                done();
-              })
-              .catch(err => {
-                console.log(err);
-                done();
-              });
-          }
-        );
       });
     });
 
@@ -107,24 +76,6 @@ describe("routes : topics", () => {
           expect(err).toBeNull();
           expect(body).toContain("JS Frameworks");
           done();
-        });
-      });
-    });
-
-    describe("POST /topics/:id/destroy", () => {
-      it("should delete the topic with the associated ID", done => {
-        Topic.findAll().then(topics => {
-          const topicCountBeforeDelete = topics.length;
-
-          expect(topicCountBeforeDelete).toBe(1);
-
-          request.post(`${base}${this.topic.id}/destroy`, (err, res, body) => {
-            Topic.findAll().then(topics => {
-              expect(err).toBeNull();
-              expect(topics.length).toBe(topicCountBeforeDelete - 1);
-              done();
-            });
-          });
         });
       });
     });
@@ -140,23 +91,65 @@ describe("routes : topics", () => {
       });
     });
 
+    describe("POST /topics/create", () => {
+      const options = {
+        url: `${base}create`,
+        form: {
+          title: "blink-182 songs",
+          description: "What's your favorite blink-182 song?"
+        }
+      };
+
+      it("should create a new topic and redirect", done => {
+        request.post(options, (err, res, body) => {
+          Topic.findOne({ where: { title: "blink-182 songs" } })
+            .then(topic => {
+              expect(topic.title).toBe("blink-182 songs");
+              expect(topic.description).toBe(
+                "What's your favorite blink-182 song?"
+              );
+              done();
+            })
+            .catch(err => {
+              console.log(err);
+              done();
+            });
+        });
+      });
+    });
+
+    describe("POST /topics/:id/destroy", () => {
+      it("should delete the topic with the associated ID", done => {
+        Topic.all().then(topics => {
+          const topicCountBeforeDelete = topics.length;
+
+          expect(topicCountBeforeDelete).toBe(1);
+          request.post(`${base}${this.topic.id}/destroy`, (err, res, body) => {
+            Topic.all().then(topics => {
+              expect(err).toBeNull();
+              expect(topics.length).toBe(topicCountBeforeDelete);
+              done();
+            });
+          });
+        });
+      });
+    });
+
     describe("POST /topics/:id/update", () => {
-      it("should update the topic with the given values", done => {
+      it("should not update the topic with the given values", done => {
         const options = {
           url: `${base}${this.topic.id}/update`,
           form: {
-            title: "JS Frameworks",
+            title: "JavaScript Frameworks",
             description: "There are a lot of them"
           }
         };
-
         request.post(options, (err, res, body) => {
           expect(err).toBeNull();
-
           Topic.findOne({
-            where: { id: this.topic.id }
+            where: { id: 1 }
           }).then(topic => {
-            expect(topic.title).toBe("JS Frameworks");
+            expect(topic.title).toBe("JS Frameworks"); //confirm title is unchanged
             done();
           });
         });
@@ -164,9 +157,7 @@ describe("routes : topics", () => {
     });
   });
 
-  //end context for admin user
-
-  // context of member user
+  // define the member user context
   describe("member user performing CRUD actions for Topic", () => {
     beforeEach(done => {
       request.get(
@@ -177,14 +168,13 @@ describe("routes : topics", () => {
           }
         },
         (err, res, body) => {
-          //did not remove at refactoring
           done();
         }
       );
     });
 
     describe("GET /topics", () => {
-      it("should respond with all topics", done => {
+      it("should return a status code 200 and all topics", done => {
         request.get(base, (err, res, body) => {
           expect(err).toBeNull();
           expect(body).toContain("Topics");
@@ -195,35 +185,10 @@ describe("routes : topics", () => {
     });
 
     describe("GET /topics/new", () => {
-      it("should redirect to topics view", done => {
+      it("should render a new topic form", done => {
         request.get(`${base}new`, (err, res, body) => {
-          expect(err).toBeNull();
           expect(body).toContain("Topics");
           done();
-        });
-      });
-    });
-
-    describe("POST /topics/create", () => {
-      const options = {
-        url: `${base}create`,
-        form: {
-          title: "blink-182 songs",
-          description: "What's your favorite blink-182 song?"
-        }
-      };
-
-      it("should not create a new topic", done => {
-        request.post(options, (err, res, body) => {
-          Topic.findOne({ where: { title: "blink-182 songs" } })
-            .then(topic => {
-              expect(topic).toBeNull(); // no topic should be returned
-              done();
-            })
-            .catch(err => {
-              console.log(err);
-              done();
-            });
         });
       });
     });
@@ -238,23 +203,6 @@ describe("routes : topics", () => {
       });
     });
 
-    describe("POST /topics/:id/destroy", () => {
-      it("should not delete the topic with the associated ID", done => {
-        Topic.findAll().then(topics => {
-          const topicCountBeforeDelete = topics.length;
-
-          expect(topicCountBeforeDelete).toBe(1);
-
-          request.post(`${base}${this.topic.id}/destroy`, (err, res, body) => {
-            Topic.findAll().then(topics => {
-              expect(topics.length).toBe(topicCountBeforeDelete);
-              done();
-            });
-          });
-        });
-      });
-    });
-
     describe("GET /topics/:id/edit", () => {
       it("should not render a view with an edit topic form", done => {
         request.get(`${base}${this.topic.id}/edit`, (err, res, body) => {
@@ -262,6 +210,50 @@ describe("routes : topics", () => {
           expect(body).not.toContain("Edit Topic");
           expect(body).toContain("JS Frameworks");
           done();
+        });
+      });
+    });
+
+    describe("POST /topics/create", () => {
+      const options = {
+        url: `${base}create`,
+        form: {
+          title: "blink-182 songs",
+          description: "What's your favorite blink-182 song?"
+        }
+      };
+
+      it("should create a new topic and redirect", done => {
+        request.post(options, (err, res, body) => {
+          Topic.findOne({ where: { title: "blink-182 songs" } })
+            .then(topic => {
+              expect(topic.title).toBe("blink-182 songs");
+              expect(topic.description).toBe(
+                "What's your favorite blink-182 song?"
+              );
+              done();
+            })
+            .catch(err => {
+              console.log(err);
+              done();
+            });
+        });
+      });
+    });
+
+    describe("POST /topics/:id/destroy", () => {
+      it("should delete the topic with the associated ID", done => {
+        Topic.all().then(topics => {
+          const topicCountBeforeDelete = topics.length;
+
+          expect(topicCountBeforeDelete).toBe(1);
+          request.post(`${base}${this.topic.id}/destroy`, (err, res, body) => {
+            Topic.all().then(topics => {
+              expect(err).toBeNull();
+              expect(topics.length).toBe(topicCountBeforeDelete);
+              done();
+            });
+          });
         });
       });
     });
