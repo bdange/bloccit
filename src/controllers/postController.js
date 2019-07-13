7143713c011efb998037ae35e133aa8918b4a6f4
bdange <benjamin.dange@gmail.com>
2019-07-13 12:48:02 +0200

    comments completed

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,15 +1,8 @@
 const postQueries = require("../db/queries.posts.js");
-const Authorizer = require("../policies/post");
 
 module.exports = {
   new(req, res, next) {
-    const authorized = new Authorizer(req.user).new();
-    if (authorized) {
-      res.render("posts/new", { topicId: req.params.topicId });
-    } else {
-      req.flash("notice", "You are not authorized to do that.");
-      res.redirect("/posts");
-    }
+    res.render("posts/new", { topicId: req.params.topicId });
   },
 
   create(req, res, next) {
